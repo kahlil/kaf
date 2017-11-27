3f02e02de43b1411feb8055a1470b478b1e4383e
Kahlil Lechelt <hello@kahlil.info>
2017-11-27 14:40:37 +0100

    real api request

--- js/util/kaf-api.js
+++ js/util/kaf-api.js
@@ -1,6 +1,8 @@
 class TastybeansApi {
   async getPlaces() {
-    return fetch('/data.json');
+    return fetch(
+      'https://api.foursquare.com/v2/venues/explore?v=20131016&ll=52.520007%2C13.404954&section=coffee&novelty=new&client_id=TQ00OORMFQF5LTW4DVXYW2XGMKWOYTHZEQNM4JLOQMJ35WVB&client_secret=IT33HI0JBAYWADFWIVKGU4XF5EBEP54G2AUMZOKBOI52II1G',
+    );
   }
 }
 
