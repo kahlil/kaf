fb3dd5e82033935d8170b8d3577e8c3f164dc94e
Kahlil Lechelt <hello@kahlil.info>
2017-11-30 09:54:30 +0100

    set foursquare endpoint

--- js/util/kaf-api.js
+++ js/util/kaf-api.js
@@ -5,37 +5,37 @@ import { getCurrentLocation } from './get-current-location.js';
 class TastybeansApi {
   async getPlaces() {
     let geo;
-    // try {
-    //   geo = await getCurrentLocation({
-    //     enableHighAccuracy: true,
-    //     timeout: 5000,
-    //     maximumAge: 0,
-    //   });
-
-    //   const response = await fetch(foursquareEndpoint(geo.coords.latitude, geo.coords.longitude));
-    //   const data = await response.json();
-    //   return data.response.groups[0].items;
-    // } catch (e) {
-    //   if (e.name == 'PositionError') {
-    //     console.log(e.message + '. code = ' + e.code);
-    //   }
-    // }
-
     try {
-      const response = await fetch('/data.json').then(function(response) {
-        if (response.ok) {
-          return response;
-        }
-        dispatch('NETWORK_ERROR', `Network response not OK, status: ${response.status}`);
-        throw new Error(`Network response not OK, status: ${response.status}`);
+      geo = await getCurrentLocation({
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
       });
+
+      const response = await fetch(foursquareEndpoint(geo.coords.latitude, geo.coords.longitude));
       const data = await response.json();
       return data.response.groups[0].items;
     } catch (e) {
-      console.log('api error', e);
-      dispatch('NETWORK_ERROR', e);
-      return Promise.reject(e);
+      if (e.name == 'PositionError') {
+        console.log(e.message + '. code = ' + e.code);
+      }
     }
+
+    // try {
+    //   const response = await fetch('/data.json').then(function(response) {
+    //     if (response.ok) {
+    //       return response;
+    //     }
+    //     dispatch('NETWORK_ERROR', `Network response not OK, status: ${response.status}`);
+    //     throw new Error(`Network response not OK, status: ${response.status}`);
+    //   });
+    //   const data = await response.json();
+    //   return data.response.groups[0].items;
+    // } catch (e) {
+    //   console.log('api error', e);
+    //   dispatch('NETWORK_ERROR', e);
+    //   return Promise.reject(e);
+    // }
   }
 }
 
